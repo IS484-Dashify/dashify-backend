791e08df234031d0be948ae35fd661a6d9366c47
lionelgwk <lionelgwk@icloud.com>
2024-03-23 23:47:48 +0800

    edited run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Start Python scripts in the background
-python app.py &
-python components.py &
-python machines.py &
-python services.py &
-python results.py &
-python thresholds.py &
-python getResults.py &
-python getStatus.py &
-python notifications.py &
-python getNames.py &
-python users.py &
+pm2 start app.py &
+pm2 start components.py &
+pm2 start machines.py &
+pm2 start services.py &
+pm2 start results.py &
+pm2 start thresholds.py &
+pm2 start getResults.py &
+pm2 start getStatus.py &
+pm2 start notifications.py &
+pm2 start getNames.py &
+pm2 start users.py &
 
 # Wait for all background jobs to finish
 wait
