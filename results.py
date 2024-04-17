069564a27166ff82217622bc005fbc7f522cf748
MikeyTheOng <michaelong.2021@scis.smu.edu.sg>
2024-04-17 11:06:39 +0800

    Refactor get_metrics_by_cid to utilize selectedDateRange in frontend Pre-process data in route before sending to frontend Adjust steps based on selectedDateRange (e.g., for a 3-hour range, return results in 3-minute intervals) Refer to minutesIntervalDict for selectedDateRange:steps ratio

--- results.py
+++ results.py
@@ -2,7 +2,7 @@ from flask import request, jsonify
 from dotenv import load_dotenv
 from helper import safe_convert
 from models import db, Results
-from helper import getStatusFromMetric
+from helper import getStatusFromMetric, isObjectWithDatetimeInArray, findHighestZeroDatetime, calSystemDowntime
 from app import app
 
 @app.route('/get-all-results', methods=['GET'])
@@ -11,18 +11,100 @@ def get_all_results():
     results = [result.json() for result in all_results]
     return jsonify({"results": results})
 
+"""
+returns {
+    msg,
+    data: {
+        CPU Usage, Disk Usage, Memory Usage, Traffic Metrics
+        Sys uptime
+        Sys DT (can be 0 if system is up)
+    }
+}
+"""
 @app.route('/get-result/<int:cid>/<int:rows>', methods=['GET'])
 def get_metrics_by_cid(cid, rows):
+    minutesIntervalDict = {
+        15: 1, # return rows every minute
+        30: 1, # return rows every minute
+        60: 1, # return rows every minute
+        180: 3, # return rows every 3 minutes = 60 rows
+        360: 5, # return rows every 5 minutes = 72 rows
+        720: 6, # return rows every 6 minutes = 120 rows
+        1440: 6, # return rows every 6 minutes = 240 rows
+        10080: 15, # return rows every 15 minutes = 672 rows (1300, 1315, 1330, 1345)
+        43200: 30, # return rows every 30 minutes = 1440 rows (1300, 1330, 1400, 1430)
+        129600: 60 # return rows every 60 minutes = 2160 rows (1300, 1400, 1500, 1600)
+    }
     try:
         results = Results.query.filter_by(cid=cid).order_by(Results.datetime.desc()).limit(rows)
-        results_json = []
+        results = []
+        response = {}
         if results:
-
             for row in results:
-                results_json.append(row.json())
-                
+                results.append(row.json())
+
+            aggregatedResults = {
+                "CPU Usage": [],
+                "Disk Usage": [],
+                "Memory Usage": [],
+                "Traffic In": [],
+                "Traffic Out": [],
+                "Traffic Metrics": []
+            }
+            tempTrafficResults = {
+                "Traffic In": [],
+                "Traffic Out": []
+            }
+            if cid != 1 and cid != 7: # if not live component
+                for i in range(0, len(results), minutesIntervalDict[rows]):
+                    aggregatedResults["CPU Usage"].append({"CPU Usage": results[i]["cpu_usage"], "Datetime": results[i]["datetime"]})
+                    aggregatedResults["Disk Usage"].append({"Disk Usage": results[i]["disk_usage"], "Datetime": results[i]["datetime"]})
+                    aggregatedResults["Memory Usage"].append({"Memory Usage": results[i]["memory_usage"], "Datetime": results[i]["datetime"]})
+                    tempTrafficResults["Traffic In"].append({"Traffic In": results[i]["traffic_in"], "Datetime": results[i]["datetime"]})
+                    tempTrafficResults["Traffic Out"].append({"Traffic Out": results[i]["traffic_out"], "Datetime": results[i]["datetime"]})
+            else:
+                for i in range(0, len(results)):
+                    aggregatedResults["CPU Usage"].append({"CPU Usage": results[i]["cpu_usage"], "Datetime": results[i]["datetime"]})
+                    aggregatedResults["Disk Usage"].append({"Disk Usage": results[i]["disk_usage"], "Datetime": results[i]["datetime"]})
+                    aggregatedResults["Memory Usage"].append({"Memory Usage": results[i]["memory_usage"], "Datetime": results[i]["datetime"]})
+                    tempTrafficResults["Traffic In"].append({"Traffic In": results[i]["traffic_in"], "Datetime": results[i]["datetime"]})
+                    tempTrafficResults["Traffic Out"].append({"Traffic Out": results[i]["traffic_out"], "Datetime": results[i]["datetime"]})
+            datetime_array = [
+                item['Datetime'] for item in tempTrafficResults['Traffic In']
+            ] + [
+                item['Datetime'] for item in tempTrafficResults['Traffic Out']
+            ]
+            unique_datetime_array = set(datetime_array)
+            sorted_datetime_array = sorted(unique_datetime_array, key=lambda x: datetime.strptime(x, '%Y-%m-%dT%H:%M:%S'))
+            aggregatedResults["Traffic Metrics"] = []
+            for datetime in sorted_datetime_array:
+                traffic_in_arr = tempTrafficResults['Traffic In']
+                traffic_out_arr = tempTrafficResults['Traffic Out']
+                if isObjectWithDatetimeInArray(traffic_in_arr, datetime) and isObjectWithDatetimeInArray(traffic_out_arr, datetime):
+                    aggregatedResults["Traffic Metrics"].append({
+                        "Traffic In": traffic_in_arr["Traffic In"],
+                        "Traffic Out": traffic_out_arr["Traffic Out"],
+                        "Datetime": datetime
+                    })
+            sys_uptime = results[0]["system_uptime"]
+            if sys_uptime == 0:
+                earliestZeroDateString = findHighestZeroDatetime(results)['datetime']
+                sys_downtime = calSystemDowntime(results[0]["datetime"], earliestZeroDateString)
+            else:
+                sys_downtime = 0
+            response = {
+                "msg": "success",
+                "data": {
+                    "CPU Usage": aggregatedResults["CPU Usage"],
+                    "Disk Usage": aggregatedResults["Disk Usage"],
+                    "Memory Usage": aggregatedResults["Memory Usage"],
+                    "Traffic Metrics": aggregatedResults["Traffic Metrics"],
+                    "System Uptime": sys_uptime,
+                    "System Downtime": sys_downtime
+                }
+            }                    
             # Return JSON response with the single result
-            return jsonify(results_json), 200
+            return jsonify(response), 200
         else:
             # Return an empty response with status code 404 if no results are found
             return jsonify({}), 404
@@ -46,6 +128,7 @@ def get_last_result(cid, mid):
         metricsList = ["disk_usage", "cpu_usage", "memory_usage"]
 
         if last_result:
+            print("System uptime:", last_result.system_uptime)
             if last_result.system_uptime == 0:
                 return jsonify({"status": "Critical"})
             
